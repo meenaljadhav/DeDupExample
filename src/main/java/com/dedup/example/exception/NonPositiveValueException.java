b9841f56cadd1eb85ba790d2fddb27f1b298defa
Meenal Jadhav <meenaljadhav@gmail.com>
2016-05-23 22:08:08 -0400

    expanded junit to cover more use cases attached pmd report created new exception class changed log levels changed debug messages for main class

---
+++ src/main/java/com/dedup/example/exception/NonPositiveValueException.java
@@ -0,0 +1,54 @@
+package com.dedup.example.exception;
+
+/**
+ * 
+ * Exception Class
+ */
+public class NonPositiveValueException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * 
+	 * Constructor
+	 */
+	public NonPositiveValueException() {
+		super();
+	}
+
+	/**
+	 * 
+	 * Constructor
+	 */
+	public NonPositiveValueException(final String arg0) {
+		super(arg0);
+	}
+	
+	/**
+	 * 
+	 * Constructor
+	 */
+	public NonPositiveValueException(final Throwable arg0) {
+		super(arg0);
+	}
+
+	/**
+	 * 
+	 * Constructor
+	 */
+	public NonPositiveValueException(final String arg0, final Throwable arg1) {
+		super(arg0, arg1);
+	}
+	
+	/**
+	 * 
+	 * Constructor
+	 */
+	public NonPositiveValueException(final String arg0, final Throwable arg1, final boolean arg2, final boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+	}
+
+}
